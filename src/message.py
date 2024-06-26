eabee9ec3037c89ece205b957d45754814c4fd7c
user0-07161 <user0thenyancat@proton.me>
2024-06-26 18:13:50 +0200

    FIXUP: Fixed a huge bug which made the bot respoding to it's own messages

--- src/message.py
+++ src/message.py
@@ -25,82 +25,74 @@ async def on_message(message):
             await message.channel.send(
                 f"You are not staff! Apply by DMing <@{owner}> with a short reason why you want to be staff."
             )
-            print(
-                f"User {message.author.id} attempted using a owner-only command.")
-    if (
-        client.user in message.mentions
-        or message.channel.id in channels
-        and not message.author.bot
-        and not message.content.startswith("/")
-    ):
-        async with message.channel.typing():
-            bot = None
-            try:
-                cookies = json.loads(
-                    open(
-                        f"{Path.cwd()}/bing_cookies.json",
-                        encoding="utf-8").read())
-                bot = await Chatbot.create(cookies=cookies)
-                if not message.attachments:
-                    response_bing = await bot.ask(
-                        prompt=message.content,
-                        conversation_style=ConversationStyle.precise,
-                        simplify_response=True,
+            print(f"User {message.author.id} attempted using a owner-only command.")
+    if client.user in message.mentions or message.channel.id in channels:
+        if not message.author.bot:
+            async with message.channel.typing():
+                bot = None
+                try:
+                    cookies = json.loads(
+                        open(f"{Path.cwd()}/bing_cookies.json", encoding="utf-8").read()
                     )
-                else:
-                    msg = await message.channel.send("🔎Analyzing your image...")
-                    attachmentdsc = message.attachments[0]
-                    response_bing = await bot.ask(
-                        prompt=message.content,
-                        conversation_style=ConversationStyle.precise,
-                        simplify_response=True,
-                        attachment={"image_url": f"{attachmentdsc.url}"},
-                    )
-                    await msg.edit("✅Done!")
-                nonparsed = json.dumps(
-                    response_bing, indent=2, ensure_ascii=False)
-                parsed_done = json.loads(nonparsed)
-                actualtext = str(parsed_done["text"])
-                for match in re.findall(
-                    "Analysing the (?:.......??) (.)aces may be (........)to(................)",
-                    actualtext,
-                ):
-                    actualtext.replace(str(match), "")
-                global chats
-                chats = chats + 1
-                chat_log = float(chats)
-                await message.channel.send(actualtext, reference=message)
-                if re.search(
-                    r"ll ?try ?to ?create ?that",
-                        parsed_done["text"]):
-                    async with message.channel.typing():
-                        piccookie = ""
-                        for cookie in cookies:
-                            if cookie["name"] == "_U":
-                                piccookie = cookie["value"]
-                                break
-                        sync_gen = ImageGen(auth_cookie=piccookie, quiet=True)
-                        image = sync_gen.get_images(prompt=message.content)
-                        sync_gen.save_images(image, "images")
-                        images = []
-                        for image in os.listdir("images")[:1]:
-                            img = f = os.path.join("images", image)
-                            if os.path.isfile(img):
-                                images.append(discord.File(
-                                    img, filename="image.jpeg"))
-                        embed = discord.Embed()
-                        embed.set_image(url="attachment://image.jpeg")
-                        await message.channel.send(
-                            files=images, embed=embed, reference=message
+                    bot = await Chatbot.create(cookies=cookies)
+                    if not message.attachments:
+                        response_bing = await bot.ask(
+                            prompt=message.content,
+                            conversation_style=ConversationStyle.precise,
+                            simplify_response=True,
                         )
-                        for image in os.listdir("images"):
-                            img = f = os.path.join("images", image)
-                            os.remove(img)
-            except Exception as error:
-                raise error
-                await message.channel.send(
-                    f"Oops, an error occured! Please contact the owner at <@{client.owner_id}>"
-                )
-            finally:
-                if bot is not None:
-                    await bot.close()
+                    else:
+                        msg = await message.channel.send("🔎Analyzing your image...")
+                        attachmentdsc = message.attachments[0]
+                        response_bing = await bot.ask(
+                            prompt=message.content,
+                            conversation_style=ConversationStyle.precise,
+                            simplify_response=True,
+                            attachment={"image_url": f"{attachmentdsc.url}"},
+                        )
+                        await msg.edit("✅Done!")
+                    nonparsed = json.dumps(response_bing, indent=2, ensure_ascii=False)
+                    parsed_done = json.loads(nonparsed)
+                    actualtext = str(parsed_done["text"])
+                    for match in re.findall(
+                        "Analysing the (?:.......??) (.)aces may be (........)to(................)",
+                        actualtext,
+                    ):
+                        actualtext.replace(str(match), "")
+                    global chats
+                    chats = chats + 1
+                    chat_log = float(chats)
+                    await message.channel.send(actualtext, reference=message)
+                    if re.search(r"ll ?try ?to ?create ?that", parsed_done["text"]):
+                        async with message.channel.typing():
+                            piccookie = ""
+                            for cookie in cookies:
+                                if cookie["name"] == "_U":
+                                    piccookie = cookie["value"]
+                                    break
+                            sync_gen = ImageGen(auth_cookie=piccookie, quiet=True)
+                            image = sync_gen.get_images(prompt=message.content)
+                            sync_gen.save_images(image, "images")
+                            images = []
+                            for image in os.listdir("images")[:1]:
+                                img = f = os.path.join("images", image)
+                                if os.path.isfile(img):
+                                    images.append(
+                                        discord.File(img, filename="image.jpeg")
+                                    )
+                            embed = discord.Embed()
+                            embed.set_image(url="attachment://image.jpeg")
+                            await message.channel.send(
+                                files=images, embed=embed, reference=message
+                            )
+                            for image in os.listdir("images"):
+                                img = f = os.path.join("images", image)
+                                os.remove(img)
+                except Exception as error:
+                    raise error
+                    await message.channel.send(
+                        f"Oops, an error occured! Please contact the owner at <@{owner}>"
+                    )
+                finally:
+                    if bot is not None:
+                        await bot.close()
