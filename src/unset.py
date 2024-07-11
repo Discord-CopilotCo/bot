add8b3b29616d040f2457dd5b17333f85d5f085c
User0 <104380885+user0-07161@users.noreply.github.com>
2024-07-11 10:09:03 +0200

    Update unset.py

--- src/unset.py
+++ src/unset.py
@@ -1,8 +1,9 @@
 @client.slash_command(description="Unset channel for chatting with copilot :(")
 async def unset(interaction: discord.Interaction):
-    if interaction.channel.id in channels:
-        channels.remove(interaction.channel.id)
-        updatechannelsjson()
-        await interaction.response.send_message("Unset.")
-    else:
-        await interaction.response.send_message("This channel is not set.")
+    if interaction.user.guild_permissions.manage_guild:
+        if interaction.channel.id in channels:
+            channels.remove(interaction.channel.id)
+            updatechannelsjson()
+            await interaction.response.send_message("Unset.")
+        else:
+            await interaction.response.send_message("This channel is not set.")
