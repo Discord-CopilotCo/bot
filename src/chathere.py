1a88d0ad4a42bba8f384cd10e2e621529137a2e6
User0 <104380885+user0-07161@users.noreply.github.com>
2024-07-11 10:07:42 +0200

    Update chathere.py

--- src/chathere.py
+++ src/chathere.py
@@ -1,13 +1,14 @@
 @client.slash_command(description="Set this channel for the bot to talk here :)")
 async def chathere(interaction: discord.Interaction):
-    global channels
-    if interaction.channel.id not in channels:
-        channels.append(interaction.channel.id)
-        updatechannelsjson()
-        await interaction.response.send_message(
-            "Channel set successfully! Have fun! \;-)"
-        )
-    else:
-        await interaction.response.send_message(
-            f"Oops, this channel is set already! If it doesn't works, contact the support team or DM <@{owner}>"
-        )
+    if interaction.user.guild_permissions.manage_guild:
+        global channels
+        if interaction.channel.id not in channels:  
+            channels.append(interaction.channel.id)
+            updatechannelsjson()
+            await interaction.response.send_message(
+                "Channel set successfully! Have fun! \;-)"
+            ) 
+        else:
+            await interaction.response.send_message(
+                f"Oops, this channel is set already! If it doesn't works, contact the support team or DM <@{owner}>"
+            )
